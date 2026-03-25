d5121b01af867292c493b3ddad24d8de2c5e2018
John Alvin Rivera <johnalvin2121@gmail.com>
2026-03-25 12:44:28 +0800

    finishing the admin and controller routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
+
+import { clerkMiddleware } from '@clerk/express'
+import fileUpload from 'express-fileupload';
+import path from 'path';
 import { connectDB } from './lib/db.js';
 
 import userRoutes from './routes/user.routes.js';
@@ -11,10 +15,19 @@ import statsRoutes from './routes/stat.route.js';
 
 dotenv.config();
 
+const __dirname = path.resolve();
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(clerkMiddleware());
+app.use(fileUpload({
+  useTempFiles: true,
+  tempFileDir: path.join(__dirname, 'tmp'),
+  createParentPath: true,
+  limits: { fileSize: 10 * 1024 * 1024 }, 
+}));
+
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
@@ -22,6 +35,10 @@ app.use("/api/admin", adminRoutes);
 app.use("/api/songs", songRoutes);
 app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statsRoutes);
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: process.env.NODE_ENV === 'production' ? 'Internal Server Error' : err.message });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
